de790413b3714f79a6ec496ac46eb33b9cabc3f4
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2024-09-15 23:58:54 +0200

    Update fizzbuzz0.js

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -1,9 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const form = document.getElementById("name-form");
-    const greeting = document.getElementById("greeting");
-    const outputContainer = document.getElementById("output-container");
-}
-
 document.getElementById("name-form").addEventListener("submit", function(event) {
     event.preventDefault();
     
