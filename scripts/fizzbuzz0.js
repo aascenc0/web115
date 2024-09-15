aed1736ffe8d42e589965e1769fddac0451d3c45
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2024-09-15 23:45:52 +0200

    Update fizzbuzz0.js

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -20,7 +20,7 @@ document.getElementById("name-form").addEventListener("submit", function(event) 
     }
 
     // Create full name
-    const fullName = "${firstName} ${middleInitial ? middleInitial + "."} ${lastName}".trim();
+    const fullName = "${firstName} ${middleInitial ? middleInitial + "."}" "${lastName}".trim();
     
     // Update the welcome message
     document.getElementById("welcome-message").innerText = `Welcome to Paws &amp; Pints Pub! ${fullName}!`;
