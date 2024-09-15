399e463c5080735a51578abf8a99b6d1a83b0c18
aascenc0 <128317989+aascenc0@users.noreply.github.com>
2024-09-16 01:24:59 +0200

    Update fizzbuzz0.js

--- scripts/fizzbuzz0.js
+++ scripts/fizzbuzz0.js
@@ -8,7 +8,7 @@ document.getElementById("name-form").addEventListener("submit", function(event) 
     const lastName = document.getElementById("last_name").value;
 
     // Prompt the user to enter a number between 1 and 125
-    let numberInput = parseInt(window.prompt("Please enter a number between 1 and 125:"));
+    let numberInput = parseInt(window.prompt("How high do you want to count, $("first_name")?: "));
     
     // Validate the number input
     while (numberInput < 1 || numberInput > 125 || isNaN(numberInput)) {
@@ -19,16 +19,10 @@ document.getElementById("name-form").addEventListener("submit", function(event) 
     let gameOutput = "";
     for (let i = 1; i <= numberInput; i++) {
         if (i % 2 === 0) {
-            gameOutput += "(i) - This number is Even<br>";
+            gameOutput += `Paw's &amp; Pints Pub - ${i} - This number is Even<br>`;
         } else {
-            gameOutput += "(i) - This number is Odd<br>";
+            gameOutput += `Paw's &amp; Pints Pub - ${i} - This number is Odd<br>`;
         }
-    }
-    
-    // Prepare the additional lines of output related to the theme
-    let themeOutput = "";
-    for (let i = 1; i <= 125; i++) {
-        themeOutput += "${i}. Happy Hour Drinks!<br>";
     }
 
     // Display results
